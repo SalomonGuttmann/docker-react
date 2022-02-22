61d977418a1657ba5de6f74753f5189de64cc263
root <root@localhost.localdomain>
2022-02-22 22:21:00 +0100

    initiall commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	  I I was changee!! 
         </p>
         <a
           className="App-link"
